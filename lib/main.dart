5eb272a8b63da5bf3bbc3483466d8f934f172ba3
Farah Wahida <farahwahida1232002@gmail.com>
2025-12-13 01:55:50 +0800

    ver 5.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:mydiary/loginscreen.dart';
 import 'package:mydiary/mainscreen.dart';
 import 'package:mydiary/registerscreen.dart';
 
